cc708d2eed97152338c7d7f6d03804bc72e93511
UsuarioTemporal <thomtwd@gmail.com>
2019-05-20 08:58:49 -0500

    Calibrando la frecuencia del dispositivo

--- python/day_1/day_1.py
+++ python/day_1/day_1.py
@@ -1,1 +1,9 @@
 # Calibrar el dispositivo
+# archivo = open('input.txt','r')
+# print(archivo.read())
+# archivo.close()
+
+archive = open('input.txt','r')
+for line in archive.readlines(): 
+    print(int(line))
+archive.close()
