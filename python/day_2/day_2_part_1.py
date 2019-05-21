f5106f85561a3d110952827b5239f21968f11589
UsuarioTemporal <thomtwd@gmail.com>
2019-05-21 10:37:30 -0500

    Funcion que analiza las repeticiones de los ids de las cajas

--- python/day_2/day_2_part_1.py
+++ python/day_2/day_2_part_1.py
@@ -1,6 +1,13 @@
-with open('input.txt','r') as file :
-    items = [line.strip() for line in file]
-print(items)
+# with open('input.txt','r') as file :
+#     items = [line.strip() for line in file]
+# print(items)
 
-def analyzeRepetitions():
-    pass
+ids = [line.strip() for line in open('input.txt').readlines()]
+
+def analyzeRepetitions(string)->dict:
+    values = {}
+    for letter in string :
+        values[letter] = values.get(letter,0)+1
+    return values
+
+print(analyzeRepetitions(ids))
