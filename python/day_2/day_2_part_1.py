cc7d12bb201a8eb926c567bd7aa8263cd58bd759
UsuarioTemporal <thomtwd@gmail.com>
2019-05-21 13:26:59 -0500

    Map

--- python/day_2/day_2_part_1.py
+++ python/day_2/day_2_part_1.py
@@ -3,7 +3,7 @@
 # print(items)
 
 ids = [line.strip() for line in open('input.txt').readlines()]
-ids = 'abbcc'
+ids = ['abcdef','bababc','abbcde','abcccd','aabcdd','abcdee','ababab']
 def analyzeRepetitions(string)->dict:
     values = {}
     for letter in string :
@@ -14,6 +14,7 @@ def isValid(input):
     values = analyzeRepetitions(input)
     return (2 in values.values(),3 in values.values())
 
-
-print(analyzeRepetitions(ids))
-print(isValid(ids))
+repetitions = list(map(isValid,ids))
+print(repetitions)
+# print(analyzeRepetitions(ids))
+# print(isValid(ids))
