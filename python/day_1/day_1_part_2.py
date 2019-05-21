0688fdfdb22707c93f7ac2cb075888ef0f8f958e
UsuarioTemporal <thomtwd@gmail.com>
2019-05-20 22:22:59 -0500

    Encontrar frecuencia repetida

--- python/day_1/day_1_part_2.py
+++ python/day_1/day_1_part_2.py
@@ -1,3 +1,21 @@
 with open('input_2.txt','r') as file :
     # items = file.readlines()
     items = [int(line.strip()) for line in file]
+# You notice that the device repeats the same frequency change list over
+#  and over. To calibrate the device, you need to find the first frequency 
+# it reaches twice.
+# For example, using the same list of changes above, the device would loop
+#  as follows:
+
+#     Current frequency  0, change of +1; resulting frequency  1.
+#     Current frequency  1, change of -2; resulting frequency -1.
+#     Current frequency -1, change of +3; resulting frequency  2.
+#     Current frequency  2, change of +1; resulting frequency  3.
+#     (At this point, the device continues from the start of the list.)
+#     Current frequency  3, change of +1; resulting frequency  4.
+#     Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+
+def findRepetition( ):
+    pass
+
+print(items)
