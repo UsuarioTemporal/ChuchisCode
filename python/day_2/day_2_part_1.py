58ee1807136f480ec56497ec574c5b188d6058e9
UsuarioTemporal <thomtwd@gmail.com>
2019-05-21 13:00:23 -0500

    Validando repeticiones

--- python/day_2/day_2_part_1.py
+++ python/day_2/day_2_part_1.py
@@ -3,11 +3,17 @@
 # print(items)
 
 ids = [line.strip() for line in open('input.txt').readlines()]
-
+ids = 'abbcc'
 def analyzeRepetitions(string)->dict:
     values = {}
     for letter in string :
         values[letter] = values.get(letter,0)+1
     return values
 
+def isValid(input): 
+    values = analyzeRepetitions(input)
+    return (2 in values.values(),3 in values.values())
+
+
 print(analyzeRepetitions(ids))
+print(isValid(ids))
