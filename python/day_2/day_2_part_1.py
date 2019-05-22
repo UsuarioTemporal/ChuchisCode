90974f2f6b95b7a9f7d724bf9088ff6e120f1eb8
UsuarioTemporal <thomtwd@gmail.com>
2019-05-21 22:45:24 -0500

    Checksum completed

--- python/day_2/day_2_part_1.py
+++ python/day_2/day_2_part_1.py
@@ -1,9 +1,9 @@
 # with open('input.txt','r') as file :
 #     items = [line.strip() for line in file]
 # print(items)
-
+from functools import reduce
 ids = [line.strip() for line in open('input.txt').readlines()]
-ids = ['abcdef','bababc','abbcde','abcccd','aabcdd','abcdee','ababab']
+# ids = ['abcdef','bababc','abbcde','abcccd','aabcdd','abcdee','ababab']
 def analyzeRepetitions(string)->dict:
     values = {}
     for letter in string :
@@ -15,6 +15,10 @@ def isValid(input):
     return (2 in values.values(),3 in values.values())
 
 repetitions = list(map(isValid,ids))
-print(repetitions)
+
+boxesWith2 = reduce(lambda acc,tup: acc+1 if tup[0]==True else acc,repetitions,0)
+boxesWith3 = reduce(lambda acc,tup:acc+1 if tup[-1]==True else acc,repetitions,0)
+# print(repetitions)
+print(boxesWith2*boxesWith3)
 # print(analyzeRepetitions(ids))
 # print(isValid(ids))
